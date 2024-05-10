86d8c06eca0116f0496563e1dce11055127cd00d
Aura Tordjman <tordjmanaura@gmail.com>
2024-05-10 20:28:26 +0200

    Ajout champs Nom/Prénom

--- src/app/(main)/Inscription/page.tsx
+++ src/app/(main)/Inscription/page.tsx
@@ -20,7 +20,9 @@ const EyeOffIcon = () => (
 const SignupForm = () => {
   const me  = useAuthContext(); // Retrieve user context
 
-  
+  const [firstName, setFirstName] = useState("");
+const [lastName, setLastName] = useState("");
+
   const [registerEmail, setRegisterEmail]= useState("");
   const [registerPassword, setRegisterPassword]= useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
@@ -54,41 +56,44 @@ const SignupForm = () => {
   console.log(me)
   return (
     <div className="max-w-md mx-auto mt-8 p-6 bg-gray-100 rounded-lg shadow-md">
-      
-
-      <h2 className="text-2xl font-semibold text-gray-800 mb-4">Inscription</h2>
-      <form action="/submit" method="post">
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700 font-medium mb-1">Email :</label>
-          <input type="email" id="email" name="email" className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500" required onChange={(event)=> {setRegisterEmail(event.target.value);}}/>
-        </div>
-        <label htmlFor="password1" className="block text-gray-700 font-medium mb-1 mr-3">Mot de passe :</label>
-        <div className="mb-4 relative flex items-center">
-  
-  <div className="flex-grow relative">
-    <input type={passwordVisible ? "text" : "password"} 
-           id="password1" 
-           name="password1" 
-           className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500" 
-           required 
-           onChange={(event) => setRegisterPassword(event.target.value)}
-    />
-    <button
-      type="button"
-      onClick={() => setPasswordVisible(!passwordVisible)}
-      className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
-      aria-label={passwordVisible ? "Cacher le mot de passe" : "Afficher le mot de passe"}
-    >
-      {passwordVisible ? <EyeOffIcon /> : <EyeIcon />}
-    </button>
-  </div>
+  <h2 className="text-2xl font-semibold text-gray-800 mb-4">Inscription</h2>
+  <form action="/submit" method="post">
+    <div className="mb-4">
+      <label htmlFor="firstName" className="block text-gray-700 font-medium mb-1">Prénom :</label>
+      <input type="text" id="firstName" name="firstName" className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500" required onChange={(event) => setFirstName(event.target.value)}/>
+    </div>
+    <div className="mb-4">
+      <label htmlFor="lastName" className="block text-gray-700 font-medium mb-1">Nom :</label>
+      <input type="text" id="lastName" name="lastName" className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500" required onChange={(event) => setLastName(event.target.value)}/>
+    </div>
+    <div className="mb-4">
+      <label htmlFor="email" className="block text-gray-700 font-medium mb-1">Email :</label>
+      <input type="email" id="email" name="email" className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500" required onChange={(event) => setRegisterEmail(event.target.value)}/>
+    </div>
+    <label htmlFor="password1" className="block text-gray-700 font-medium mb-1 mr-3">Mot de passe :</label>
+    <div className="mb-4 relative flex items-center">
+      <div className="flex-grow relative">
+        <input type={passwordVisible ? "text" : "password"} 
+               id="password1" 
+               name="password1" 
+               className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500" 
+               required 
+               onChange={(event) => setRegisterPassword(event.target.value)}
+        />
+        <button
+          type="button"
+          onClick={() => setPasswordVisible(!passwordVisible)}
+          className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
+          aria-label={passwordVisible ? "Cacher le mot de passe" : "Afficher le mot de passe"}
+        >
+          {passwordVisible ? <EyeOffIcon /> : <EyeIcon />}
+        </button>
+      </div>
+    </div>
+    <button type="button" className="w-full bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 transition duration-300" onClick={register}>S&apos;inscrire</button>
+  </form>
 </div>
 
-        <button type="button" className="w-full bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 transition duration-300"  onClick={register} >S&apos;inscrire</button>
-
-
-      </form>
-    </div>
   );
 };//
 export default SignupForm;
